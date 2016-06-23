45db33a626ae3d40eb03767c139f8e9d13574ef0
Kelly Lampotang <klampotang@fb.com>
2016-06-22 22:54:10 -0700

    Sticky headers

--- instaG/FeedViewController.swift
+++ instaG/FeedViewController.swift
@@ -55,13 +55,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.didReceiveMemoryWarning()
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return 6
-        return instaposts.count
+        return 1
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return instaposts.count ?? 0;
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("feedCell", forIndexPath: indexPath) as! FeedCell
         
-        let textPfObject = self.instaposts[indexPath.row]
+        let textPfObject = self.instaposts[indexPath.section]
         // get text string out of the pf object
         if let stringText = textPfObject.valueForKey("caption") {
             let dateNS = textPfObject.createdAt
@@ -124,7 +126,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = tableView.dequeueReusableHeaderFooterViewWithIdentifier(HeaderViewIdentifier)! as UITableViewHeaderFooterView
-        header.textLabel!.text = "hi"
+        let lilPFObject = instaposts[section]
+        let PFUser = lilPFObject.valueForKey("author")
+        let stringUser = PFUser?.username
+        header.textLabel!.text = stringUser!
         return header
     }
     
